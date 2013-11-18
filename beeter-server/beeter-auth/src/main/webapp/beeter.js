939e242d7ff9571f71d5a49282d7cf20c7c6da0d
rodrigo.sampedro <ropnom5291@gmail.com>
2013-11-18 13:52:54 +0100

    No funciona ...

--- beeter-server/beeter-auth/src/main/webapp/beeter.js
+++ beeter-server/beeter-auth/src/main/webapp/beeter.js
@@ -1,48 +1,49 @@
-var API_BASE_URL = "http://localhost:8000/better-api";
+
+//definimos variables a utilizar
+var API_BASE_URL = "http://localhost:8000/beeter-api";
+var user = 'alicia';
+var pass  ='alicia';
+var auten = 'alicia:aliacia';
  
  
+//evento de accion sobre el boton
 $("#button_get_sting").click(function(e){
 	e.preventDefault();
-	getSting(85);
+	getSting(46);
 });
  
+//evento de accion sobre el boton
 $("#button_delete_sting").click(function(e){
 	e.preventDefault();
-	deleteSting(85);
+	deleteSting(45);
 });
- 
+
+//evento de accion sobre el boton
 $("#button_post_sting").click(function(e){
 	e.preventDefault();
 	var sting ='{"content": "Muchos años después, frente al pelotón de fusilamiento...", "subject": "Cien Años de Soledad", "username": "alicia"}';
 	createSting(sting);
 });
  
- 
+ //funcion obtener un sting realiza la peticion json a beeter-api
 function getSting(stingid) {
-	//var url = "/stings/"+stingid;
-	var url = "http://localhost:8000/better-api/stings/85";
+	var url = "http://127.0.0.1:8000/beeter-api/stings/41";
  
+	//creamos el json Accept que interoga a beterapi y envia la apeticion
 	$.ajax({
 		url : url,
 		type : 'GET',
-		crossDomain : true,
-		dataType : 'json',
 		username : 'alicia',
 		password : 'alicia',
-		headers : {
-			"Accept" : "application/vnd.beeter.api.sting+json",
-			//"Access-Control-Allow-Origin" : "*"
-		},
-		beforeSend: function (request)
-	    {
-	        request.withCredentials = true;
-	        request.setRequestHeader("Authorization", "Basic "+ btoa('alicia:alicia'));
-	    },
+		crossDomain : true,		
 	})
+	//?si la respuesta es buena
 	.done(function (data, status, jqxhr) {
+		//lee la respuesta  la introduce a uan variable y la pega en consola del navegador
 		var sting = $.parseJSON(jqxhr.responseText);
 		console.log(sting);
 	})
+	//si la respuesta es negativa pega el error en la consola del navegador
     .fail(function (jqXHR, textStatus) {
 		console.log(textStatus);
 	});
@@ -56,8 +57,8 @@ function deleteSting(stingid) {
 		url : url,
 		type : 'DELETE',
 		crossDomain : true,
-		username : 'username',
-		password : 'password',
+		username : user,
+		password : pass,
 	})
     .done(function (data, status, jqxhr) {
 		console.log(status);
@@ -74,14 +75,14 @@ function createSting(sting) {
  
 	$.ajax({
         dataType: "jsonp",
-        url: "http://jsfiddle.net/echo/jsonp/",
+        url: url,
 		// url : url,
 		type : 'POST',
 		crossDomain : true,
 		data : sting,
 		//dataType : 'json',
-		username : 'username',
-		password : 'password',
+		username : user,
+		password : pass,
 	})
 	.done(function (data, status, jqxhr) {
 		console.log(status);
